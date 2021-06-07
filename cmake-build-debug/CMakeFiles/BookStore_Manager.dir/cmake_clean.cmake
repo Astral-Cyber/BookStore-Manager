a3d54a2cf079b93cd58684a13b66078c18646752
奉眠 <33065020+Astral-cyber@users.noreply.github.com>
2021-06-07 14:02:48 +0800

    建立

---
+++ cmake-build-debug/CMakeFiles/BookStore_Manager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BookStore_Manager"
+  "BookStore_Manager.pdb"
+  "CMakeFiles/BookStore_Manager.dir/main.cpp.o"
+  "CMakeFiles/BookStore_Manager.dir/shelf.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookStore_Manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
